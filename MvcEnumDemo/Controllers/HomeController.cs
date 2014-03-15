bbeddb434ccbecf5b00ca6eff5e5f702fd39cfdc
itorian <abhikumarvatsa@yahoo.co.in>
2014-03-15 18:11:33 +0530

    initial commit

---
+++ MvcEnumDemo/Controllers/HomeController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MvcEnumDemo.Models;
+
+namespace MvcEnumDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: /Home/
+        public ActionResult Index()
+        {
+            return View(db.Friends.ToList());
+        }
+
+        // GET: /Home/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Friend friend = db.Friends.Find(id);
+            if (friend == null)
+            {
+                return HttpNotFound();
+            }
+            return View(friend);
+        }
+
+        // GET: /Home/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: /Home/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include="Id,Salutation,Name,Mobile")] Friend friend)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Friends.Add(friend);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(friend);
+        }
+
+        // GET: /Home/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Friend friend = db.Friends.Find(id);
+            if (friend == null)
+            {
+                return HttpNotFound();
+            }
+            return View(friend);
+        }
+
+        // POST: /Home/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include="Id,Salutation,Name,Mobile")] Friend friend)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(friend).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(friend);
+        }
+
+        // GET: /Home/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Friend friend = db.Friends.Find(id);
+            if (friend == null)
+            {
+                return HttpNotFound();
+            }
+            return View(friend);
+        }
+
+        // POST: /Home/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Friend friend = db.Friends.Find(id);
+            db.Friends.Remove(friend);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ MvcEnumDemo/Models/Friend.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MvcEnumDemo.Models
+{
+    public class Friend
+    {
+        public int Id { get; set; }
+        public Salutation Salutation { get; set; }
+        public string Name { get; set; }
+        public string Mobile { get; set; }
+    }
+
+    public enum Salutation
+    {
+        [Display(Name = "Mr.")]
+        Mr,
+        [Display(Name = "Mrs.")]
+        Mrs,
+        [Display(Name = "Ms.")]
+        Ms,
+        [Display(Name = "Dr.")]
+        Doctor,
+        [Display(Name = "Prof.")]
+        Professor,
+        Sir,
+        Lady,
+        Lord
+    }
+}
